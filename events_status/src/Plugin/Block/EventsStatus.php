93f079b13402141e6e3b722c43957d534c4db52b
Aljaz-S <52696644+Aljaz-S@users.noreply.github.com>
2020-07-24 20:52:20 +0200

    Main configuration updated

--- events_status/src/Plugin/Block/EventsStatus.php
+++ events_status/src/Plugin/Block/EventsStatus.php
@@ -8,14 +8,14 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Session\AccountInterface;
 
 /**
- * Provides a block with a simple text.
+ * Provides a block with events status content.
  *
  * @Block(
- *   id = "my_block_example_block",
- *   admin_label = @Translation("My block"),
+ *   id = "events_status",
+ *   admin_label = @Translation("Events Status"),
  * )
  */
-class MyBlock extends BlockBase {
+class EventsStatus extends BlockBase {
   /**
    * {@inheritdoc}
    */
@@ -26,16 +26,13 @@ class MyBlock extends BlockBase {
 	$content = "";
 	
 	# SQL to fetch dates and titles of all events, if getting result the while function is executed process recived data
-	$sql = "SELECT c.title AS Title, d.field_event_date_value AS Date FROM node__field_event_date d INNER JOIN node_field_data c ON c.nid = d.entity_id order by d.field_event_date_value desc;";
-	$result = db_query($sql);
+	$sql = "SELECT c.title AS Event_Title, d.field_event_date_value AS Event_Date FROM node__field_event_date d INNER JOIN node_field_data c ON c.nid = d.entity_id order by d.field_event_date_value desc;";
+	$result = db_query($sql)->fetchAll();
 	
-	#TO DO: Properly check if you got any values from DB.
-	if ($result) {
-		while ($row = $result->fetchAssoc()) {
-		
-			$event_title = $row['Title'];
-			$event_date = $row['Date'];
-			$datetime2 = date_create(date("Y-m-d", strtotime($event_date)));
+	if (!empty($result)) {		
+		foreach($result as $value){
+			
+			$datetime2 = date_create(date("Y-m-d", strtotime($value->Event_Date)));
 			$interval = date_diff($datetime1, $datetime2);
 			$day_diff = $interval->format("%R%a");
 			
@@ -46,16 +43,15 @@ class MyBlock extends BlockBase {
 			*	- Event happening today display: This event is happening today.
 			*/
 			if ($day_diff == 0){
-				$content .= "<h1>" . $event_title . " </h1> <p>Date: " . $row['Date'] . "</p><p>This event is happening today</p><hr/>";
+				$content .= "<h1>" . $value->Event_Title . " </h1> <p>Date: " . $value->Event_Date . "</p><p>This event is happening today</p><hr/>";
 			} else if ($day_diff < 0) {
-				$content .= "<h1>" . $event_title . "</h1> <p>Date: " . $row['Date'] . "</p><p> This event already passed.</h4><p><hr/>";
+				$content .= "<h1>" . $value->Event_Title . "</h1> <p>Date: " . $value->Event_Date . "</p><p> This event already passed.</h4><p><hr/>";
 			} else if ($day_diff > 0) {
-				$content .= "<h1>" . $event_title . "</h1> <p>Date: " . $row['Date'] . "</p><p>" . $day_diff . " days left until event starts</p><hr/>";
-			} #TO DO: Add else to report error or something else	
-		
+				$content .= "<h1>" . $value->Event_Title . "</h1> <p>Date: " . $value->Event_Date . "</p><p>" . $day_diff . " days left until event starts</p><hr/>";
+			}
 		}
 	} else {
-		$content = "There is no event data available at the moment!";
+		$content = "There is not event information available!";
 	}
 	
     return [
